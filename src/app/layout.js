23ddaa81e8cded4bd3bfd8259e0c918b2dd8cdbc
Himanshi <himanshijangid444@gmail.com>
2026-01-16 09:34:34 +0530

    Add Google Analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -83,7 +83,8 @@ export default function RootLayout({ children }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-K8554W2S6Y');
+            gtag('config', 'G-6CB0R515JB
+');
           `}
         </Script>
       </head>
